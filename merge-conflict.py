93ea4209ae770b234ebb99ec7ee664b317001739
Rachael <ballentine.7@wright.edu>
2024-05-20 12:20:31 -0400

    update merge-conflict

--- merge-conflict.py
+++ merge-conflict.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-print("hello there")
+print ("hello there")
 
 print("conflict here")
 
-print("general kenobi")
-=======
-print("hello there general kenobi")
-
-print("conflict here")
-
-print("general grevious")
-
->>>>>>> 9dedbd00bbab5739c7b6728b6ec35ebb4afa17c5
+print("general kenobi") 
