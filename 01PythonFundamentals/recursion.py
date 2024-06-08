b43207b482eecd5ecc7e858588f16fce3ceda1b1
rballentine25 <ballentine.7@wright.edu>
2024-06-07 22:54:17 -0400

    recursion py script

---
+++ 01PythonFundamentals/recursion.py
@@ -0,0 +1,47 @@
+def Fib(n): 
+    if n <= 0:
+        return 0
+    elif n ==1:
+        return 1
+    else:
+        return Fib(n - 1) + Fib(n - 2)
+    
+print("fibonacci of 4 is:", Fib(4))
+
+
+def S(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 2
+    else: 
+        return S(n-1)**2
+print("S(4) is:", S(4))
+
+
+recList = None
+def recaman(n, seq = None):
+    global recList
+    if seq is None:
+        seq = [0]
+    
+    if n == 1:
+        if 1 not in seq:
+            seq.append(1)
+
+        return 1
+    else:
+        prev = recaman(n-1, seq)
+        newRec = prev - n
+        
+        if newRec <= 0 or newRec in seq:
+            newRec = prev + n
+        
+        seq.append(newRec)
+        recList = seq[:]
+        return newRec
+recaman(10)
+print("first 10 numbers of Recaman's sequence are:\n", recList)
+
+
+
