409db74bdb5e4ff91fc1b0e5725f4edbf220948f
rballentine25 <ballentine.7@wright.edu>
2025-02-11 00:10:44 -0500

    saving standard scaler mod

--- 08ProjectData/tsne_wildfire_NOTJUP.py
+++ 08ProjectData/tsne_wildfire_NOTJUP.py
@@ -4,28 +4,37 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
 import time
+import matplotlib.colors as mcolors
 
 start = time.time()
 
 #leave at this one to show dr raymer (this was weirdest). takes about 46s
-df2 = pd.read_excel("mid-process-data-files/TO-COMBINE/train_07_rearranged.xlsx")
+df2 = pd.read_excel("mid-process-data-files/TO-COMBINE/train_05_rearranged.xlsx")
 df2.head()
 
 # for tSNE, X is the features and Y is the labels (here just firemask)
-X2 = df2.iloc[:, 1:]
-Y2 = df2.iloc[:,0]
+X = df2.iloc[:, 2:] # start inclusive: this excludes firemask AND prev fire mask
+Y = df2.iloc[:,0]
+
+scaler = StandardScaler()
+Xs = scaler.fit_transform(X)
 
 n_dims = 3      # dimensions of the embedded space
 tsne = TSNE(n_dims)
-result2 = tsne.fit_transform(X2)
+result = tsne.fit_transform(Xs)
 #print(result.shape)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-result2_df = pd.DataFrame({'TSNE_col1': result2[:,0], 'TSNE_col2': result2[:,1], 'TSNE_col3': result2[:,2], 'label' : Y2})
-ax.scatter3D(result2_df['TSNE_col1'], result2_df['TSNE_col2'], result2_df['TSNE_col3'], c=Y2, cmap='viridis')
+result_df = pd.DataFrame({'TSNE_col1': result[:,0], 'TSNE_col2': result[:,1], 'TSNE_col3': result[:,2], 'label' : Y})
+
+color_map = {-1:'cyan', 0:'gray', 1:'orange'}
+color_num = result_df['label'].map(color_map)
+
+ax.scatter3D(result_df['TSNE_col1'], result_df['TSNE_col2'], result_df['TSNE_col3'], c=color_num, cmap=mcolors.ListedColormap(list(color_map.values())))
 end = time.time()
 totaltime = end-start
 print(totaltime)
